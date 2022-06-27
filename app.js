9d982f757f44fc38963d5c42228309453095b474
1Regina <1reginacheong@gmail.com>
2022-06-27 14:00:24 +0800

    feat: order updates, details, admin, records

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 const braintreeRoutes = require("./routes/braintree");
+const orderRoutes = require("./routes/order");
 
 // app
 const app = express();
@@ -45,6 +46,7 @@ app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api", braintreeRoutes);
+app.use("/api", orderRoutes);
 
 const port = process.env.PORT || 8000; // nodejs env we get process to run it
 
