d3cd63d1b1028c64b14bbe1dddc30beea419e5fb
1Regina <1reginacheong@gmail.com>
2022-06-28 21:34:18 +0800

    feat: Manage products by Admin

--- routes/product.js
+++ routes/product.js
@@ -41,6 +41,7 @@ router.get("/products/categories", listCategories);
 router.post("/products/by/search", listBySearch);
 router.get("/product/photo/:productId", photo);
 
+
 router.param("userId", userById);
 router.param("productId", productById);
 
